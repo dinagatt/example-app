708f8987702fe6d5db7f951c7a271bc52825c2a6
dinagatt <dina.abdeeva@gmail.com>
2026-03-08 21:06:02 +0100

    fix: fix the greeting in AppService2

--- src/app.service2.ts
+++ src/app.service2.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService2 {
   getHello(): string {
-    return `What's app, World!`;
+    return `What's up, World!`;
   }
 }
